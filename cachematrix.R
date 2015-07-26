6107059628938376d600b1c42f83e40412a24266
Courtney Hoskins <choskins@e0400.local>
2015-07-26 01:04:39 -0600

    caching exercies

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions essentially share data outside of their scope 
+## using the <<- operator
 
-## Write a short comment describing this function
+## This will create a list containing a function to set and get the value
+## of a matrix and its inverse. 
+## example: mat <- matrix(c(1,3,2,4), nrow= 2, ncol =2)
+## cachedMat <- makeCacheMatrix(mat)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This will make use of the list of functions in the previous function
+## to calculate the inverse of the matrix. If it is already calculated,
+## it will grab the inverse from the cache. If not, it will calculate it
+## and set it in cache. Using the above example:
+## cacheSolve(cachedMat) will return either the calculated inverse, or a
+## message that it is retrieving the value from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
